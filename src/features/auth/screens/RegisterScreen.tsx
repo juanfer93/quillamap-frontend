f8b97ed7e77779f38f05e9030fb0869793780e84
dabestjuan1993 <dabestjuan1993@gmail.com>
2026-05-06 15:40:05 +0000

    feat: mejoras

--- src/features/auth/screens/RegisterScreen.tsx
+++ src/features/auth/screens/RegisterScreen.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, TextInput, useColorScheme, SafeAreaView, ActivityIndicator } from 'react-native';
 import { RegisterRequest, RegisterResponse } from '../types/auth.types';
+import { FontAwesome } from '@expo/vector-icons';
+
 
 // Colores corporativos
 const corporateColors = {
@@ -96,15 +98,19 @@ const RegisterScreen = () => {
           <View style={styles.stepContainer}>
             <Text style={styles.questionText}>¿Cómo te mueves por la ciudad?</Text>
             <TouchableOpacity style={styles.card} onPress={() => handleVehicleTypeSelect('peaton')}>
+                <FontAwesome name="male" size={40} color={isDarkMode ? corporateColors.gold : corporateColors.shark} />
               <Text style={styles.cardText}>Peatón</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.card} onPress={() => handleVehicleTypeSelect('turista')}>
+            <FontAwesome name="user-circle" size={40} color={isDarkMode ? corporateColors.gold : corporateColors.shark} />
               <Text style={styles.cardText}>Turista</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.card} onPress={() => handleVehicleTypeSelect('moto')}>
+            <FontAwesome name="motorcycle" size={40} color={isDarkMode ? corporateColors.gold : corporateColors.shark} />
               <Text style={styles.cardText}>Moto</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.card} onPress={() => handleVehicleTypeSelect('carro')}>
+            <FontAwesome name="car" size={40} color={isDarkMode ? corporateColors.gold : corporateColors.shark} />
               <Text style={styles.cardText}>Carro</Text>
             </TouchableOpacity>
           </View>
@@ -114,9 +120,11 @@ const RegisterScreen = () => {
           <View style={styles.stepContainer}>
             <Text style={styles.questionText}>¿Qué tipo de carro conduces?</Text>
             <TouchableOpacity style={styles.card} onPress={() => handleCarTypeSelect('particular')}>
+            <FontAwesome name="circle-o" size={24} color={isDarkMode ? corporateColors.gold : corporateColors.shark} />
               <Text style={styles.cardText}>Particular</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.card} onPress={() => handleCarTypeSelect('taxi')}>
+            <FontAwesome name="circle-o" size={24} color={isDarkMode ? corporateColors.gold : corporateColors.shark} />
               <Text style={styles.cardText}>Taxi</Text>
             </TouchableOpacity>
           </View>
@@ -202,11 +210,14 @@ const getStyles = (isDarkMode: boolean) => StyleSheet.create({
     shadowOpacity: 0.1,
     shadowRadius: 4,
     elevation: 3,
+    flexDirection: 'row',
+    justifyContent: 'center',
   },
   cardText: {
     fontSize: 18,
     fontWeight: '600',
     color: isDarkMode ? corporateColors.white : corporateColors.shark,
+    marginLeft: 15,
   },
   plate: {
       width: '80%',
